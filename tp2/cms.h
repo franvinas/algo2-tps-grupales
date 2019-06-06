c9fd1d24bab020abf251293f7f2088530e2a1a71
Fran <fvinas@fi.uba.ar>
2019-06-05 22:59:18 -0300

    Creo el archivo cms.h con algunas de las funciones

---
+++ tp2/cms.h
@@ -0,0 +1,28 @@
+#ifndef _CMS_H
+#define _CMS_H
+
+typedef struct cms cms_t;
+
+/* Crea el cms
+ */
+cms_t* cms_crear(size_t tam);
+
+/* Incrementa en uno la cantidad de apariciones de la clave,
+ * Pre: La estructura cms fue inicializada
+ * Post: Se incremento el valor
+ */
+bool cms_guardar(cms_t* cms, const char* clave);
+
+/* Devuelve la cantidad de apariciones de la clave (aproximada)
+ * Pre: La estructura cms fue inicializada
+ */
+size_t cms_obtener(const cms_t *cms, const char *clave);
+
+/* Destruye la estructura liberando la memoria pedida
+ * Pre: La estructura cms fue inicializada
+ * Post: La estructura cms fue destruida
+ */
+void cms_destruir(cms_t* cms);
+
+
+#endif // CMS_H
